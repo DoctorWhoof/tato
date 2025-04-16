d9d9c66829d5518d917d1bae8ef9f942af0286b6
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2025-04-15 23:35:38 -0700

    Tweaked 9bit colors

--- src/color.rs
+++ src/color.rs
@@ -46,38 +46,58 @@ pub const PINK: ColorID = ColorID(15);
 pub(crate) const PALETTE_DEFAULT: [Color9Bit; 16] = [
     Color9Bit::new(0, 0, 0), // BG, 0
     Color9Bit::new(0, 0, 0), // Black, 1
-    Color9Bit::new(3, 3, 3), // Gray, 2
-    Color9Bit::new(6, 6, 6), // White, 3
-    Color9Bit::new(3, 1, 0), // Dark Red, 4
+    Color9Bit::new(4, 4, 4), // Gray, 2
+    Color9Bit::new(7, 7, 7), // White, 3
+    Color9Bit::new(3, 0, 0), // Dark Red, 4
     Color9Bit::new(5, 2, 2), // Red, 5
-    Color9Bit::new(6, 4, 4), // Light Red, 6
-    Color9Bit::new(5, 3, 0), // Orange, 7
-    Color9Bit::new(6, 5, 1), // Yellow, 8
-    Color9Bit::new(1, 2, 1), // Dark Green, 9
-    Color9Bit::new(1, 4, 1), // Green, 10
-    Color9Bit::new(4, 5, 3), // Green Light, 11
-    Color9Bit::new(1, 1, 3), // Dark Blue, 12
-    Color9Bit::new(2, 2, 6), // Blue, 13
-    Color9Bit::new(3, 5, 6), // Light Blue, 14
-    Color9Bit::new(5, 2, 4), // Pink, 15
+    Color9Bit::new(7, 5, 5), // Light Red, 6
+    Color9Bit::new(6, 4, 1), // Orange, 7
+    Color9Bit::new(7, 6, 3), // Yellow, 8
+    Color9Bit::new(0, 2, 1), // Dark Green, 9
+    Color9Bit::new(2, 4, 2), // Green, 10
+    Color9Bit::new(4, 6, 3), // Green Light, 11
+    Color9Bit::new(0, 1, 2), // Dark Blue, 12
+    Color9Bit::new(1, 2, 5), // Blue, 13
+    Color9Bit::new(4, 6, 7), // Light Blue, 14
+    Color9Bit::new(6, 3, 6), // Pink, 15
 ];
+
 
 // /// "Pure" 3 bits per channel palette. Very bright and saturated.
 // pub const PALETTE_DEFAULT: [Color9Bit; 16] = [
 //     Color9Bit::new(0, 0, 0), // BG, 0
 //     Color9Bit::new(0, 0, 0), // Black, 1
-//     Color9Bit::new(4, 4, 4), // Gray, 2
+//     Color9Bit::new(3, 3, 3), // Gray, 2
 //     Color9Bit::new(7, 7, 7), // White, 3
 //     Color9Bit::new(3, 1, 1), // Dark Red, 4
 //     Color9Bit::new(5, 1, 1), // Red, 5
-//     Color9Bit::new(7, 4, 3), // Light Red, 6
+//     Color9Bit::new(6, 4, 3), // Light Red, 6
 //     Color9Bit::new(5, 3, 0), // Orange, 7
-//     Color9Bit::new(7, 7, 1), // Yellow, 8
+//     Color9Bit::new(6, 6, 1), // Yellow, 8
 //     Color9Bit::new(1, 2, 1), // Dark Green, 9
 //     Color9Bit::new(1, 4, 1), // Green, 10
-//     Color9Bit::new(3, 7, 3), // Green Light, 11
+//     Color9Bit::new(3, 6, 3), // Green Light, 11
 //     Color9Bit::new(1, 1, 3), // Dark Blue, 12
-//     Color9Bit::new(1, 2, 7), // Blue, 13
-//     Color9Bit::new(3, 7, 7), // Light Blue, 14
-//     Color9Bit::new(5, 2, 5), // Pink, 15
+//     Color9Bit::new(1, 2, 6), // Blue, 13
+//     Color9Bit::new(3, 6, 6), // Light Blue, 14
+//     Color9Bit::new(6, 2, 6), // Pink, 15
+// ];
+
+// pub const PALETTE_ORIGINAL: [ColorRGB24; 16] = [
+//     ColorRGB24 { r:   0, g:   0, b:   0 }, // BG, 0
+//     ColorRGB24 { r:  10, g:  10, b:  10 }, // Black, 1
+//     ColorRGB24 { r: 122, g: 122, b: 122 }, // Gray, 2
+//     ColorRGB24 { r: 228, g: 228, b: 228 }, // White, 3
+//     ColorRGB24 { r: 110, g:  42, b:  20 }, // Dark Red, 4
+//     ColorRGB24 { r: 192, g:  64, b:  64 }, // Red, 5
+//     ColorRGB24 { r: 212, g: 156, b: 148 }, // Light Red, 6
+//     ColorRGB24 { r: 178, g: 126, b:  20 }, // Orange, 7
+//     ColorRGB24 { r: 212, g: 202, b:  46 }, // Yellow, 8
+//     ColorRGB24 { r:  42, g:  72, b:  48 }, // Dark Green, 9
+//     ColorRGB24 { r:  46, g: 154, b:  46 }, // Green, 10
+//     ColorRGB24 { r: 140, g: 202, b: 126 }, // Green Light, 11
+//     ColorRGB24 { r:  40, g:  40, b: 126 }, // Dark Blue, 12
+//     ColorRGB24 { r:  64, g:  72, b: 212 }, // Blue, 13
+//     ColorRGB24 { r: 124, g: 176, b: 228 }, // Light Blue, 14
+//     ColorRGB24 { r: 188, g:  88, b: 148 }, // Pink, 15
 // ];
