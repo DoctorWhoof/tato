7d798210d75ca1a68ab004b3d643f88b019dc07c
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2025-04-17 21:16:02 -0700

    Pan and Volume queue (only process on cycle change)

--- src/channel.rs
+++ src/channel.rs
@@ -2,7 +2,7 @@ use core::ops::RangeInclusive;
 
 use libm::roundf;
 
-use crate::{waveform::*, math::*,rng::*, *};
+use crate::{math::*, rng::*, waveform::*, *};
 
 const FREQ_C4: f32 = 261.63;
 
@@ -10,7 +10,7 @@ const FREQ_C4: f32 = 261.63;
 const TONE_FREQ_STEPS: u16 = 4096;
 const NOISE_FREQ_STEPS: u16 = 4096;
 const NOISE_PITCH_MULTIPLIER: f32 = 16.0;
-const VOLUME_ATTENUATION: f32 = 0.002; // TODO: zeroed for debuggin, unzero
+const VOLUME_ATTENUATION: f32 = 0.0025;
 
 // C0 to C10 in "scientific pitch"", roughly the human hearing range
 pub const FREQ_RANGE: RangeInclusive<f32> = 16.0..=16384.0;
@@ -43,15 +43,19 @@ pub struct Channel {
     rng: Rng,
     noise_period: f32,
     noise_output: f32,
-    // Internal State
+    // Misc. Internal State
     volume_attn: f32,
-    wave_output: f32,
+    wave_level: f32, // The "voltage" of the output, drops over time and resets on every new sample
+    wave_out: f32,   // The actual output value, combines level, wavetable and volume
     left_mult: f32,
     right_mult: f32,
     last_sample_index: usize,
     last_sample_value: u4,
     phase_increment: f32,      // How much to advance phase per sample
     noise_period_samples: f32, // Noise period in samples
+    queued_volume: Option<u4>,
+    queued_pan: Option<i4>,
+    cycle_step: usize, // Zeroed out on every new cycle
 }
 
 impl Channel {
@@ -59,7 +63,7 @@ impl Channel {
     pub(crate) fn new(sample_rate: f32) -> Self {
         let mut result = Self {
             sample_rate,
-            wavetable: WAVE_TRIANGLE,
+            wavetable: WAVE_SAWTOOTH,
             noise_mode: NoiseMode::default(),
             volume: 0,
             pan: 0,
@@ -71,13 +75,17 @@ impl Channel {
             noise_period: 0.0,
             noise_output: 0.0,
             volume_attn: 1.0,
-            wave_output: 0.0,
+            wave_level: 0.0,
+            wave_out: 0.0,
             left_mult: 0.5,
             right_mult: 0.5,
             last_sample_index: 0,
             last_sample_value: 0,
             phase_increment: 0.0,
             noise_period_samples: 0.0,
+            queued_volume: None,
+            queued_pan: None,
+            cycle_step: 0,
         };
         result.set_volume(0);
         result.set_pan(0);
@@ -114,15 +122,13 @@ impl Channel {
 
     pub fn set_volume(&mut self, volume: u4) {
         let volume = volume.min(15);
-        self.volume = volume;
-        self.calculate_multipliers();
+        self.queued_volume = Some(volume);
     }
 
     /// Stereo panning, centered is zero.
     pub fn set_pan(&mut self, pan: i4) {
         let pan = pan.clamp(-7, 7);
-        self.pan = pan;
-        self.calculate_multipliers();
+        self.queued_pan = Some(pan);
     }
 
     /// Switches channel between tone and noise generation, if specs allow noise.
@@ -149,8 +155,7 @@ impl Channel {
     /// Set the channel's frequency.
     pub fn set_frequency(&mut self, frequency: f32) {
         // Quantize to simulate limited pitch steps
-        // self.frequency = quantize_range(frequency, TONE_FREQ_STEPS, FREQ_RANGE);
-        self.frequency = frequency;
+        self.frequency = quantize_range(frequency, TONE_FREQ_STEPS, FREQ_RANGE);
 
         // Calculate how much to advance phase per sample
         self.phase_increment = self.frequency / self.sample_rate as f32;
@@ -168,7 +173,18 @@ impl Channel {
     /// Returns the current sample and advances the internal phase by one sample at the configured sample rate
     pub fn next_sample(&mut self) -> Sample<f32> {
         // Always apply attenuation, so that values always drift to zero
-        self.wave_output *= self.volume_attn;
+        self.wave_level *= self.volume_attn;
+
+        // Advance phase and count cycles properly
+        self.phase += self.phase_increment;
+        self.cycle_step += 1;
+        if self.phase >= 1.0 {
+            // Calculate how many complete cycles we've advanced
+            let full_cycles = (self.phase as u32) as usize;
+            self.cycle_step = 0;
+            // Keep only the fractional part of phase
+            self.phase -= full_cycles as f32;
+        }
 
         // // Generate noise level, will be mixed later
         // if self.noise_mix > 0 {
@@ -190,39 +206,48 @@ impl Channel {
         // }
 
         // Determine wavetable index
-        // println!("phase: {}", self.phase);
         let len = self.wavetable.len();
-        let index = (self.phase * len as f32) as usize;
+        let sample_index = (self.phase * len as f32) as usize;
 
         // Obtain wavetable sample and set it to output
-        if index != self.last_sample_index {
-            self.last_sample_index = index;
-            let wave = self.wavetable[index];
+        if sample_index != self.last_sample_index {
+            self.last_sample_index = sample_index;
+            let wave = self.wavetable[sample_index];
             // Avoids resetting attenuation if value hasn't changed
             if wave != self.last_sample_value {
                 let value = wave as f32 / 15.0; // 0.0 to 1.0 range
                 // Map to (-1.0 .. 1.0) here, ensures proper attenuation over time
-                self.wave_output = (value * 2.0) - 1.0;
+                self.wave_level = (value * 2.0) - 1.0;
                 self.last_sample_value = wave;
+            }
+            if self.cycle_step == 0 {
+                let mut recalc_multipliers = false;
+                if let Some(volume) = self.queued_volume {
+                    self.volume = volume;
+                    self.queued_volume = None;
+                    recalc_multipliers = true;
+                }
+                if let Some(pan) = self.queued_pan {
+                    self.pan = pan;
+                    self.queued_pan = None;
+                    recalc_multipliers = true;
+                }
+                if recalc_multipliers {
+                    self.calculate_multipliers();
+                }
             }
         }
 
-        // Advance phase and count cycles properly
-        self.phase += self.phase_increment;
-        if self.phase >= 1.0 {
-            // Calculate how many complete cycles we've advanced
-            let complete_cycles = (self.phase as u32) as usize;
-            // Keep only the fractional part of phase
-            self.phase -= complete_cycles as f32;
-        }
-
         // Apply main volume
-        let output = self.wave_output * (self.volume as f32 / 15.0);
-
+        self.wave_out = if self.volume > 0 {
+            self.wave_level * (self.volume as f32 / 15.0)
+        } else {
+            self.wave_out * self.volume_attn
+        };
         // Return sample with volume and pan applied
         Sample {
-            left: output * self.left_mult,
-            right: output * self.right_mult,
+            left: self.wave_out * self.left_mult,
+            right: self.wave_out * self.right_mult,
         }
     }
 }
