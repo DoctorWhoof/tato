7c8b9ff88499ef7ee1ccec3eff4e5c758475afc9
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2025-08-14 10:52:38 -0700

    Better tilemap collision API

--- crates/collision/src/lib.rs
+++ crates/collision/src/lib.rs
@@ -1,4 +1,4 @@
-use tato_video::{DynTilemap, TilemapRef};
+use tato_video::DynTilemap;
 
 #[derive(Debug, Clone)]
 pub struct TileCollision {
@@ -9,20 +9,19 @@ pub struct TileCollision {
 /// Tests tilemap collisions from start_row to end_row (inclusive). Returns
 /// Some(TileCollision) if the tile is marked with the desired collision_group.
 /// Can check in either direction - if end_row < start_row, checks in reverse.
-pub fn line_to_tile_y<'a>(
-    map: TilemapRef<'a>,
+pub fn line_to_tile_y(
+    map: &impl DynTilemap,
     col: i16,
     start_row: i16,
     end_row: i16,
     collision_group: u8,
 ) -> Option<TileCollision> {
-    if col < 0 || col >= map.columns as i16 {
+    if col < 0 || col >= map.columns() as i16 {
         return None;
     }
     let check_row = |row: i16| {
         (row >= 0)
             .then(|| map.get_cell(col, row))?
-            // .filter(|cell| cell.flags.is_collider() )
             .filter(|cell| cell.group & collision_group != 0)
             .map(|_| TileCollision { col, row })
     };
@@ -36,20 +35,19 @@ pub fn line_to_tile_y<'a>(
 /// Tests tilemap collisions from start_col to end_col (inclusive). Returns
 /// Some(TileCollision) if the tile is marked with the desired collision_group.
 /// Can check in either direction - if end_col < start_col, checks in reverse.
-pub fn line_to_tile_x<'a>(
-    map: TilemapRef<'a>,
+pub fn line_to_tile_x(
+    map: &impl DynTilemap,
     start_col: i16,
     end_col: i16,
     row: i16,
     collision_group: u8,
 ) -> Option<TileCollision> {
-    if row < 0 || row >= map.rows as i16 {
+    if row < 0 || row >= map.rows() as i16 {
         return None;
     }
     let check_col = |col: i16| {
         (col >= 0)
             .then(|| map.get_cell(col, row))?
-            // .filter(|cell| cell.flags.is_collider() )
             .filter(|cell| cell.group & collision_group != 0)
             .map(|_| TileCollision { col, row })
     };
