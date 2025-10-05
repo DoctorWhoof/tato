ac76da80fd6e2710bca7f620dfa29eaebc117d8e
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2025-10-05 13:35:03 -0700

    Added integer delta timing (!) to anim_mquad example

--- examples/anim_mquad/src/main.rs
+++ examples/anim_mquad/src/main.rs
@@ -2,7 +2,16 @@ mod astro;
 
 use crate::astro::{ASTRO_TILESET, STRIP_ASTRO};
 use tato::{arena::Arena, prelude::*};
-use tato_macroquad::{macroquad::{self, input::is_quit_requested, time::{get_fps, get_frame_time}, window::{next_frame, Conf}}, tato_window_conf, MquadBackend};
+use tato_macroquad::{
+    MquadBackend,
+    macroquad::{
+        self,
+        input::is_quit_requested,
+        time::get_frame_time,
+        window::{Conf, next_frame},
+    },
+    tato_window_conf,
+};
 
 // A minimal entity that fits in a single 64 bit value! :-)
 struct Entity {
@@ -18,10 +27,6 @@ const W: u16 = 240;
 const H: u16 = 180;
 const BANK_BG: u8 = 0;
 const BANK_FG: u8 = 1;
-
-fn window_conf() -> Conf {
-    tato_window_conf("Tato Macroquad Example", 320, 240)
-}
 
 #[macroquad::main(window_conf)]
 async fn main() -> TatoResult<()> {
@@ -43,6 +48,13 @@ async fn main() -> TatoResult<()> {
     let anim_down = tato.init_anim(Anim { strip, fps: 8, rep: true, frames: [4, 5, 6, 5] })?;
     let anim_up = tato.init_anim(Anim { strip, fps: 8, rep: true, frames: [8, 9, 10, 9] })?;
 
+    // To be able to apply delta timing to entities with integer coordinates
+    // and velocities, we're using a SCALE factor - everything is scaled up
+    // internally, velocities are applied, and then scaled down at the end
+    // for drawing. This was a common technique in the 80's to apply simple
+    // physics to 16 bit integer  coordinates.
+    const SCALE: i16 = 16;
+
     // Entities.
     // TODO: Obtain anims from tileset, so that we can probe a frame
     // (which is just a tilemap) for its dimensions
@@ -50,13 +62,13 @@ async fn main() -> TatoResult<()> {
     let sprite_h = 16;
     let min_x = 0;
     let min_y = 0;
-    let max_x = W as i16 - sprite_w;
-    let max_y = H as i16 - sprite_h;
+    let max_x = (W as i16 - sprite_w) * SCALE;
+    let max_y = (H as i16 - sprite_h) * SCALE;
     let mut rng = tato::rng::Rng::new(32, 123);
 
     let mut entities: [Entity; 20] = core::array::from_fn(|_| {
-        let mut vel_x = rng.range_i32(-1, 1) as i8;
-        let vel_y = rng.range_i32(-1, 1) as i8;
+        let mut vel_x = (rng.range_i32(-1, 1) as i16 * SCALE) as i8;
+        let vel_y = (rng.range_i32(-1, 1) as i16 * SCALE) as i8;
         // Velocity is never (0,0)
         if vel_x == 0 && vel_y == 0 {
             vel_x = 1;
@@ -79,16 +91,19 @@ async fn main() -> TatoResult<()> {
         tato.frame_start(get_frame_time());
 
         for entity in &mut entities {
-            // Velocity control
-            entity.x += entity.vel_x as i16;
+            // Velocity control with delta
+            let vel_x = (entity.vel_x as f32 * tato.delta()) as i16;
+            entity.x += vel_x as i16;
             if entity.x >= max_x || entity.x <= min_x {
                 entity.vel_x *= -1;
-                entity.x += entity.vel_x as i16;
+                entity.x -= vel_x;
             }
-            entity.y += entity.vel_y as i16;
+
+            let vel_y = (entity.vel_y as f32 * tato.delta()) as i16;
+            entity.y += vel_y as i16;
             if entity.y >= max_y || entity.y <= min_y {
                 entity.vel_y *= -1;
-                entity.y += entity.vel_y as i16;
+                entity.y -= vel_y as i16;
             }
 
             // Anim control
@@ -110,20 +125,30 @@ async fn main() -> TatoResult<()> {
             // Draw!
             tato.draw_anim(
                 entity.anim,
-                SpriteBundle { x: entity.x, y: entity.y, flip_x: entity.flip, flip_y: false },
+                SpriteBundle {
+                    x: entity.x / SCALE,
+                    y: entity.y / SCALE,
+                    flip_x: entity.flip,
+                    flip_y: false,
+                },
             );
         }
 
         tato.frame_finish();
         dash.frame_present(&mut frame_arena, &mut backend, &tato);
         backend.frame_present(&mut frame_arena, &tato, &[&bg_map]);
-        if is_quit_requested(){
-            break
+        if is_quit_requested() {
+            break;
         }
         next_frame().await;
     }
     Ok(())
 }
+
+fn window_conf() -> Conf {
+    tato_window_conf("Tato Macroquad Example", 320, 240)
+}
+
 
 // Curiosity! To flicker sprites that go above the sprites-per-scanline
 // limit, you can do this:
